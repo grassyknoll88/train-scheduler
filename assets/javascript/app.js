44ee285733e8fe503a8a4a7c0088b61658388160
Benjamin Knoll <Knoll@Qui-Gon-Jinn.local>
2018-03-03 18:38:47 -0600

    final push

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,84 +1,96 @@
+$(function(){
+//initialize firebase
 var config = {
-    apiKey: "AIzaSyBJHm31MuAR_du8U2vMzVCdNb0jqMCVBu4",
-    authDomain: "train-scheduler-ku.firebaseapp.com",
-    databaseURL: "https://train-scheduler-ku.firebaseio.com",
-    projectId: "train-scheduler-ku",
-    storageBucket: "train-scheduler-ku.appspot.com",
-    messagingSenderId: "401502931969"
-  };
-  firebase.initializeApp(config);
+	apiKey: "AIzaSyBJHm31MuAR_du8U2vMzVCdNb0jqMCVBu4",
+	authDomain: "train-scheduler-ku.firebaseapp.com",
+	databaseURL: "https://train-scheduler-ku.firebaseio.com",
+	projectId: "train-scheduler-ku",
+	storageBucket: "train-scheduler-ku.appspot.com",
+	messagingSenderId: "401502931969"
+};
+firebase.initializeApp(config);
 
 var database = firebase.database();
-var currentTime = moment();
 
-database.ref().on("child_added", function(childSnap) {
 
-    var name = childSnap.val().name;
-    var destination = childSnap.val().destination;
-    var firstTrain = childSnap.val().firstTrain;
-    var frequency = childSnap.val().frequency;
-    var min = childSnap.val().min;
-    var next = childSnap.val().next;
 
-    $("#trainTable > tbody").append("<tr><td>" + name + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + next + "</td><td>" + min + "</td></tr>");
-});
-
-database.ref().on("value", function(snapshot) {
-   
-
-});
 
 //retrieves info from the form
-$("#addTrainBtn").on("click", function() {
+$("#addTrainBtn").on("click", function () {
+	event.preventDefault();
 
-    var trainName = $("#trainNameInput").val().trim();
-    var destination = $("#destinationInput").val().trim();
-    var firstTrain = $("#timeInput").val().trim();
-    var frequency = $("#frequencyInput").val().trim();
+	var trainName = $("#trainNameInput").val().trim();
+	var destination = $("#destinationInput").val().trim();
+	var firstTrain = $("#timeInput").val().trim();
+	var frequency = $("#frequencyInput").val().trim();
 
-    //makes sure that each input has a value
-    if (trainName == "") {
-        alert("Enter a train name.");
-        return false;
-    }
-    if (destination == "") {
-        alert("Enter a destination.");
-        return false;
-    }
-    if (firstTrain == "") {
-        alert("Enter a train time.");
-        return false;
-    }
-    if (frequency == "") {
-        alert("Enter a frequency");
-        return false;
-    }
+	//makes sure that each input has a value
+	if (trainName == "") {
+		alert("Enter a train name.");
+		return false;
+	}
+	if (destination == "") {
+		alert("Enter a destination.");
+		return false;
+	}
+	if (firstTrain == "") {
+		alert("Enter a train time.");
+		return false;
+	}
+	if (frequency == "") {
+		alert("Enter a frequency");
+		return false;
+	}
 
-    // Here is the math for the scheduler
-    //subtracts the first train time back a year to ensure it's before current time.
-    var firstTrainConverted = moment(firstTrain, "hh:mm").subtract("1, years");
-    // the time difference between current time and the first train
-    var difference = currentTime.diff(moment(firstTrainConverted), "minutes");
-    var remainder = difference % frequency;
-    var minUntilTrain = frequency - remainder;
-    var nextTrain = moment().add(minUntilTrain, "minutes").format("hh:mm a");
+	database.ref().push({
+		name: name,
+		destination: destination,
+		firstTrain: firstTrain,
+		frequency: frequency,
+		min: minUntilTrain,
+		nextArrival: nextArrival
+	});
 
-    var newTrain = {
-        name: trainName,
-        destination: destination,
-        firstTrain: firstTrain,
-        frequency: frequency,
-        min: minUntilTrain,
-        next: nextTrain
-    }
-
-    console.log(newTrain);
-    database.ref().push(newTrain);
-
-    $("#trainNameInput").val("");
-    $("#destinationInput").val("");
-    $("#timeInput").val("");
-    $("#frequencyInput").val("");
-
-    return false;
+	
 });
+
+	database.ref().on("child_added", function (snapshot) {
+
+		var name = snapshot.val().name;
+		var destination = snapshot.val().destination;
+		var firstTrain = snapshot.val().firstTrain;
+		var frequency = snapshot.val().frequency;
+		var min = snapshot.val().min;
+		var nextArrival = snapshot.val().nextArrival;
+		var tr = $("<tr>");
+		$("tbody").append(tr);
+		tr.append("<td>" + name + "</td>");
+		tr.append("<td>" + destination + "</td>");
+		tr.append("<td>" + frequency + "</td>");
+
+	
+	// Here is the math for the scheduler
+	//subtracts the first train time back a year to ensure it's before current time.
+	var firstTrainConverted = moment(firstTrain, "hh:mm").subtract("1, years");
+	var currentTime = moment();
+	// the time difference between current time and the first train
+	var difference = moment().diff(moment(firstTrainConverted), "minutes");
+	var remainder = difference % frequency;
+	var minUntilTrain = frequency - remainder;
+	moment(minUntilTrain).minutes();
+	var nextArrival = moment().add(minUntilTrain, "minutes").format("hh:mm a");
+	tr.append("<td>" + nextArrival + "</td>");
+	tr.append("<td>" + minUntilTrain + "</td>");
+
+
+
+	$("#trainNameInput").val("");
+	$("#destinationInput").val("");
+	$("#timeInput").val("");
+	$("#frequencyInput").val("");
+
+	return false;
+});
+//refreshes train data every minute
+
+ });
